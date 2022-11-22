e632f8378a11f96feb732a99f70f15a6dc27db70
root <hsyhhssyy@gmail.com>
2022-11-22 23:11:26 +0800

    水过了功能

--- main.py
+++ main.py
@@ -2,96 +2,119 @@ import asyncio
 import sqlite3
 import os
 import re
+import dhash
+import time
 
+from io import BytesIO
+from PIL import Image
 from amiyabot import AmiyaBot, Message, Chain, log , PluginInstance
-
-try:
-    from wordcloud import WordCloud
-    enabled = True
-except ModuleNotFoundError:
-    enabled = False
+from amiyabot.network.download import download_async
 
 curr_dir = os.path.dirname(__file__)
+max_disc = 25
+
+class EmojiStatPluginInstance(PluginInstance):
+    def install(self):
+        if not os.path.exists(f'{curr_dir}/../../resource/emoji-stat/emoji'):
+            os.makedirs(f'{curr_dir}/../../resource/emoji-stat/emoji')
+        
+        if not os.path.exists(f'{curr_dir}/../../resource/emoji-stat/image'):
+            os.makedirs(f'{curr_dir}/../../resource/emoji-stat/image')
+
+        if not os.path.exists(f'{curr_dir}/../../resource/emoji-stat/emoji-stat.db'):  
+            conn = sqlite3.connect(f'{curr_dir}/../../resource/emoji-stat/emoji-stat.db')
+            c = conn.cursor()
+            c.execute('''CREATE TABLE EMOJI_STAT
+                (IMAGE_HASH     TEXT    NOT NULL,
+                SEND_COUNT      INT     NOT NULL,
+                SEND_TIME       NUMBER     NOT NULL,
+                CHANNEL_ID      TEXT    NOT NULL,
+                LAST_SENDER     TEXT    NOT NULL);''')
+            conn.commit()
+
+bot = EmojiStatPluginInstance(
+    name='图片记录员',
+    version='1.0',
+    plugin_id='amiyabot-hsyhhssyy-emoji-stat',
+    plugin_type='',
+    description='让兔兔可以收集群友的消息图片并统计emoji和火星图',
+    document=f'{curr_dir}/README.md'
+)
 
 async def any_talk(data: Message):
-    
-    # log.info('AnyTalk Collect Word Cloud')
 
-    #收集好分词后的群友句子
-    words = data.text_words
+    #计算所有的Hash
+    for image_item in data.image:
+        imgBytes = await download_async(image_item)
+        if imgBytes:
+            image = BytesIO(imgBytes)
+            try:
+                hash_value = dhash.dhash_int(Image.open(image))
+                
+                length = image.getbuffer().nbytes
 
-    #以Sqlite的形式存到fileStorage下面
-    if os.path.exists(f'{curr_dir}/../../resource/word_cloud.db'):
-        conn = sqlite3.connect(f'{curr_dir}/../../resource/word_cloud.db')
-    else:        
-        conn = sqlite3.connect(f'{curr_dir}/../../resource/word_cloud.db')
-        c = conn.cursor()
-        c.execute('''CREATE TABLE WORD_CLOUD
-            (WORD           TEXT    NOT NULL,
-            USER_ID         INT     NOT NULL,
-            QUANTITY        INT     NOT NULL);''')
-        conn.commit()
-    
-    user_id = data.user_id
+                if length > 20 * 1024 :
+                    file_path = f'{curr_dir}/../../resource/emoji-stat/image/{hash_value}.jpg'
+                else:
+                    file_path = f'{curr_dir}/../../resource/emoji-stat/emoji/{hash_value}.jpg'
 
-    c = conn.cursor()
-    for word in words:
-        # 获取当前Quantity
-        c.execute("select QUANTITY from WORD_CLOUD where USER_ID = ? and WORD = ?",(user_id,word))
-        if len(c.fetchall()) <=0 :
-            c.execute('INSERT INTO WORD_CLOUD (USER_ID,WORD,QUANTITY) values (?,?,1)' ,(user_id,word))
-        else:
-            c.execute('UPDATE WORD_CLOUD SET QUANTITY = QUANTITY +1 where USER_ID = ? and WORD = ?' ,(user_id,word))
+                with open(file_path, mode='wb+') as src:
+                    src.write(imgBytes)
 
-    conn.commit()
+                if length > 20 * 1024 :
+                    await check_image(hash_value, file_path, data)
+                else:
+                    await check_emoji(hash_value, file_path, data)
+
+                log.info(f'处理了一张图片{file_path}')
+            except OSError:
+                continue
 
     return False,0
 
-class WordCloudPluginInstance(PluginInstance):
-    def install(self):
-        if not os.path.exists(f'{curr_dir}/../../resource/word_cloud'):
-            os.makedirs(f'{curr_dir}/../../resource/word_cloud')
+async def check_emoji(hash_value, file_path, data):
+    pass
 
-bot = WordCloudPluginInstance(
-    name='词云统计',
-    version='1.1',
-    plugin_id='amiyabot-hsyhhssyy-wordcloud',
-    plugin_type='',
-    description='让兔兔可以统计群用户的词云',
-    document=f'{curr_dir}/README.md'
-)
+async def check_image(hash_value, file_path, data):
+
+    if os.path.exists(f'{curr_dir}/../../resource/emoji-stat/emoji-stat.db'):
+        conn = sqlite3.connect(f'{curr_dir}/../../resource/emoji-stat/emoji-stat.db')
+    else:
+        return
+    
+    user_id = data.user_id
+    channel_id = data.channel_id
+    now = time.time()
+    delta = now - 5 * 60 * 60
+    
+    c = conn.cursor()
+    str_hash = f'{hash_value}'
+    c.execute("select SEND_TIME from EMOJI_STAT where IMAGE_HASH = ? and CHANNEL_ID = ?",[str_hash,channel_id])
+
+    all_rows = c.fetchall()
+    if len(all_rows) > 0:
+        l_time = all_rows[0][0]
+
+        if l_time > delta:
+            await data.send(Chain(data).text(f'博士，这张图已经在{time.strftime("%H:%M:%S",time.localtime(l_time))}水过了。'))
+
+        c.execute("UPDATE EMOJI_STAT SET SEND_COUNT = SEND_COUNT +1,SEND_TIME = ?,LAST_SENDER= ? where IMAGE_HASH = ? and  and CHANNEL_ID = ?",
+            [now, user_id, f'{hash_value}',channel_id])
+        
+        conn.commit()
+    else:
+        #进行hash比较
+        c.execute("select IMAGE_HASH,SEND_TIME from EMOJI_STAT where SEND_TIME > ?  and CHANNEL_ID = ?",[delta,channel_id])
+        for row in c:
+            compare_hash =  int(row[0])
+            diff = dhash.get_num_bits_different(hash_value, compare_hash)
+            if diff <= max_disc:
+                await data.send(Chain(data).text(f'博士，这张图已经在{time.strftime("%H:%M:%S",time.localtime(row[1]))}水过了。'))
+
+        c.execute('INSERT INTO EMOJI_STAT (IMAGE_HASH,SEND_TIME,SEND_COUNT,LAST_SENDER,CHANNEL_ID) values (?,?,1,?,?)' ,
+            [f'{hash_value}',now,user_id,channel_id])
+        conn.commit()
 
 @bot.on_message(verify=any_talk, check_prefix=False)
 async def _(data: Message):
     return
-
-@bot.on_message(keywords=['查看词云','查询词云'], level = 5)
-async def check_wordcloud(data: Message):
-
-    # log.info('Create Word Cloud')
-
-    if not os.path.exists(f'{curr_dir}/../../resource/word_cloud.db') :
-        return Chain(data).text('兔兔的词云功能没有开放哦。')
-    
-    if not enabled :
-        return Chain(data).text('兔兔目前还不会绘制词云图片，请管理员安装对应依赖。')
-
-    user_id = data.user_id
-
-    conn = sqlite3.connect(f'{curr_dir}/../../resource/word_cloud.db')
-    c = conn.cursor()
-    c.execute(f"select QUANTITY,WORD from WORD_CLOUD where USER_ID = '{user_id}'")
-
-    frequencies = {}
-    for row in c:
-        if f'{row[1]}' not in stop_words:
-            frequencies[row[1]]=row[0]
-
-    if len(frequencies) <=0 :
-        return Chain(data).text('还没有收集到您的记录，请让我多听一会儿。')
-
-    # wordcloud = WordCloud(font_path =  "fileStorage/GenJyuuGothic-Normal-2.ttf").generate_from_frequencies(frequencies)    
-    wordcloud = WordCloud(font_path =  f'{curr_dir}/resource/msyh.ttf').generate_from_frequencies(frequencies)
-    wordcloud.to_file(f'{curr_dir}/../../resource/word_cloud/word_cloud_{data.user_id}.jpg')
-
-    return Chain(data).text('你的词云是：').image(f'{curr_dir}/../../resource/word_cloud/word_cloud_{data.user_id}.jpg')
